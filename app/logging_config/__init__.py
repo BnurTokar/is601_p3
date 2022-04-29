8a71a9a32e2260fa131a1c64be375d749dccb565
beyzanurtokar <bt268@njit.edu>
2022-04-29 16:57:51 -0500

    updated logging_config

--- app/logging_config/__init__.py
+++ app/logging_config/__init__.py
@@ -5,10 +5,31 @@ from logging.config import dictConfig
 import flask
 from flask import request, current_app
 
-from app.logging_config.log_formatters import RequestFormatter
+#from app.logging_config.log_formatters import RequestFormatter
 from app import config
 
 log_con = flask.Blueprint('log_con', __name__)
+
+import logging
+from flask import has_request_context, request
+
+
+class RequestFormatter(logging.Formatter):
+    def format(self, record):
+        if has_request_context():
+            record.url = request.url
+            record.remote_addr = request.remote_addr
+            record.request_method = request.method
+            record.request_path = request.path
+            record.ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+            record.host = request.host.split(':', 1)[0]
+            record.args = dict(request.args)
+
+        else:
+            record.url = None
+            record.remote_addr = None
+
+        return super().format(record)
 
 
 #@log_con.before_app_request
